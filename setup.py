81907cd62cf7f631e46836026c514e47c51874be
Jakub Konka <kubkon@jakubkonka.com>
2022-01-25 14:30:25 +0100

    Package the bindings

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='python-clang',
+    version='0.0',
+    description="Standalone LLVM's Clang Python bindings",
+    author="Jakub Konka",
+    author_email="kubkon@jakubkonka.com",
+    packages=["clang"],
+)
